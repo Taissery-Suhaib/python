c905ff2be9ee115f4a56521ec082d225c4d3e8c3
Suhaib Taissery <134582331+Taissery-Suhaib@users.noreply.github.com>
2023-07-22 17:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Built by batch 1 under the guidance of Hari Prasad Sir ! '
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
